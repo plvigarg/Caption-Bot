87be60efb849f959f997ff3a7380700656d34c8a
plvigarg <pallavi312k@gmail.com>
2021-12-26 18:05:13 +0530

    updates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, redirect, request
 import Caption_it as cap
-
+import os
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
     result_dic = {}
+    dir = 'static'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+        
     if request.method == 'POST':
 
         f = request.files['userfile']
